dfa9a00488914e8eb71e4421d4855c7b71837b0a
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-03-01 20:20:17 +0100

    add API face-detection

--- src/Components/ImageLink/ImageLink.js
+++ src/Components/ImageLink/ImageLink.js
@@ -11,7 +11,7 @@ const ImageLink = ({ onInputChange, onButtonSubmit }) => {
             <div className='form center pa4 br3 shadow-5'>
             <input 
             className='f4 pa2 w-70 center' 
-            type='text' 
+            type='tex' 
             onChange={onInputChange}
             />
             <button 
